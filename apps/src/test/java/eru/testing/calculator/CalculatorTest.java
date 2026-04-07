98be919d72b8251313fa47c4738d5978ceef6045
Omur <omur@erciyes.edu.tr>
2026-04-07 15:15:41 +0300

    update

--- apps/src/test/java/eru/testing/calculator/CalculatorTest.java
+++ apps/src/test/java/eru/testing/calculator/CalculatorTest.java
@@ -3,6 +3,8 @@ package eru.testing.calculator;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
+import java.util.InputMismatchException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -32,6 +34,84 @@ public class CalculatorTest {
         calculator.calculate();
 
         verify(writer).write("10.0 + 20.0 = 30.0");
+
+    }
+
+    @Test
+    void shouldMinusCorrrectly(){
+        Reader reader = mock(Reader.class);
+        Writer writer = mock(Writer.class);
+
+        when(reader.readDouble()).thenReturn(20.0, 10.0);
+        when(reader.readWord()).thenReturn("-");
+        Calculator calculator = new Calculator(reader,writer);
+
+        calculator.calculate();
+
+        verify(writer).write("20.0 - 10.0 = 10.0");
+
+    }
+
+    @Test
+    void shouldMultiplyCorrrectly(){
+        Reader reader = mock(Reader.class);
+        Writer writer = mock(Writer.class);
+
+        when(reader.readDouble()).thenReturn(20.0, 10.0);
+        when(reader.readWord()).thenReturn("*");
+        Calculator calculator = new Calculator(reader,writer);
+
+        calculator.calculate();
+
+        verify(writer).write("20.0 * 10.0 = 200.0");
+
+    }
+
+    @Test
+    void shouldDivideCorrrectly(){
+        Reader reader = mock(Reader.class);
+        Writer writer = mock(Writer.class);
+
+        when(reader.readDouble()).thenReturn(20.0, 10.0);
+        when(reader.readWord()).thenReturn("/");
+        Calculator calculator = new Calculator(reader,writer);
+
+        calculator.calculate();
+
+        verify(writer).write("10.0 / 2.0 = 5.0");
+
+    }
+
+    @Test
+    void shouldNonMathOperatorMessageCorrrectly(){
+        Reader reader = mock(Reader.class);
+        Writer writer = mock(Writer.class);
+
+        when(reader.readDouble()).thenReturn(20.0, 10.0);
+        when(reader.readWord()).thenReturn("Ersoy", "*");
+        Calculator calculator = new Calculator(reader,writer);
+
+        calculator.calculate();
+
+        verify(writer).write("Please try again (enter an operator):");
+
+    }
+
+    @Test
+    void shouldNonValidDouble(){
+        Reader reader = mock(Reader.class);
+        Writer writer = mock(Writer.class);
+
+        when(reader.readDouble())
+                .thenThrow(new InputMismatchException())
+                .thenReturn(20.0, 10.0);
+
+        when(reader.readWord()).thenReturn("*");
+        Calculator calculator = new Calculator(reader,writer);
+
+        calculator.calculate();
+
+        verify(writer).write("Please try again (enter a number):");
 
     }
 }
