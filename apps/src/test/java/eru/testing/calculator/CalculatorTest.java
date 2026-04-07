ed849bf634b1fb7aa1860179bd4548faa3d86042
Omur <omur@erciyes.edu.tr>
2026-04-07 14:37:55 +0300

    test

--- apps/src/test/java/eru/testing/calculator/CalculatorTest.java
+++ apps/src/test/java/eru/testing/calculator/CalculatorTest.java
@@ -1,8 +1,10 @@
 package eru.testing.calculator;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class CalculatorTest {
     @Test
@@ -16,5 +18,20 @@ public class CalculatorTest {
         Calculator calculator = new Calculator(reader,writer);
         calculator.calculate();
         assertEquals("10.0 + 10.0 = 20.0", writer.getValue());
+    }
+
+    @Test
+    void shouldPlusCorrrectly(){
+        Reader reader = mock(Reader.class);
+        Writer writer = mock(Writer.class);
+
+        when(reader.readDouble()).thenReturn(10.0, 20.0);
+        when(reader.readWord()).thenReturn("+");
+        Calculator calculator = new Calculator(reader,writer);
+
+        calculator.calculate();
+
+        verify(writer).write("10.0 + 20.0 = 30.0");
+
     }
 }
